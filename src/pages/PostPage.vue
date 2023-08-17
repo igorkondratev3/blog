d4376beac9da984e3482cd4041e6db3aba39ff1e
igor <igor93-333@yandex.ru>
2023-08-17 10:18:58 +0300

    add delete and edit post

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,11 +1,20 @@
 <script setup>
 import { ref, computed } from "vue";
+import { useRouter } from "vue-router/composables";
+import FullDescription from "@/components/CreatePost/postCreationForm/inputs/fullDescription.vue";
+import ShortDescription from "@/components/CreatePost/postCreationForm/inputs/shortDescription.vue";
+import TitleInput from "@/components/CreatePost/postCreationForm/inputs/titleInput.vue";
 
 const props = defineProps({
   id: String,
 });
+
+let id = props.id;
+const router = useRouter();
+
 const posts = JSON.parse(localStorage.getItem("posts"));
-const post = ref(structuredClone(posts[props.id]));
+const post = ref(structuredClone(posts[id]));
+//прявязать в модел сюда
 
 const userName = ref("");
 const userEmail = ref("");
@@ -39,7 +48,7 @@ const addComment = () => {
     date: correctDate,
   };
   post.value.comments.push(comm);
-  posts[props.id].comments.push(comm);
+  posts[id].comments.push(comm);
   localStorage.setItem("posts", JSON.stringify(posts));
   userName.value = "";
   userEmail.value = "";
@@ -49,20 +58,69 @@ const addComment = () => {
 const deleteComment = (commentIndex) => {
   console.log(commentIndex);
   post.value.comments.splice(commentIndex, 1);
-  posts[props.id].comments.splice(commentIndex, 1);
+  posts[id].comments.splice(commentIndex, 1);
   localStorage.setItem("posts", JSON.stringify(posts));
+};
+
+const deletePost = () => {
+  posts.splice(id, 1);
+  localStorage.setItem("posts", JSON.stringify(posts));
+  router.push("/");
+};
+
+const editMode = ref(false);
+const isCorrectEdit = computed(
+  () =>
+    Boolean(post.value.title) &&
+    Boolean(post.value.shortDescription) &&
+    post.value.title.length <= 50 &&
+    post.value.shortDescription.length <= 100 &&
+    post.value.fullDescription.length <= 255
+);
+
+
+const editPost = () => {
+  const date = new Date();
+  let correctDate = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()}`;
+  correctDate = correctDate
+    .split("-")
+    .map((dateP) => (dateP.length === 1 ? "0" + dateP : dateP))
+    .join("-");
+  post.value.date = correctDate;
+
+  posts.splice(id, 1);
+  posts.push(post.value);
+  localStorage.setItem("posts", JSON.stringify(posts));
+  id = posts.length - 1;
+  editMode.value = false;
 };
 </script>
 
 <template>
   <div class="page">
     <article class="post-cont">
-      <h2 class="title-p">{{ post.title }}</h2>
-      <p class="short-p">{{ post.shortDescription }}</p>
-      <p class="full-p">{{ post.fullDescription }}</p>
+      <h2 v-if="!editMode" class="title-p">{{ post.title }}</h2>
+      <TitleInput v-else v-model="post.title" />
+      <p v-if="!editMode" class="short-p">{{ post.shortDescription }}</p>
+      <ShortDescription v-else v-model="post.shortDescription" />
+      <p v-if="!editMode" class="full-p">{{ post.fullDescription }}</p>
+      <FullDescription v-else v-model="post.fullDescription" />
       <p class="date-p">{{ post.date }}</p>
       <div class="post-buttons">
-        <button class="base-button">Удалить запись</button>
+        <button class="base-button" @click="deletePost">Удалить запись</button>
+        <button class="base-button" v-if="!editMode" @click="editMode = true">
+          Редактировать запись
+        </button>
+        <button
+          class="base-button"
+          v-else
+          @click="editPost"
+          :disabled="!isCorrectEdit"
+        >
+          Закончить редактирование
+        </button>
       </div>
     </article>
     <div class="commentsss">
@@ -196,6 +254,7 @@ const deleteComment = (commentIndex) => {
   border: calc(var(--base) * 0.01) solid transparent;
   border-radius: calc(var(--base) * 0.04);
   font: inherit;
+  font-size: 20px;
   letter-spacing: inherit;
   color: inherit;
   background-color: white;
@@ -206,14 +265,15 @@ const deleteComment = (commentIndex) => {
 
 .com-con {
   width: calc(var(--base) * 7.5);
-  height: calc(var(--base) * 3);
+  height: calc(var(--base) * 1);
   max-width: 100%;
-  max-height: calc(var(--base) * 5);
+  max-height: calc(var(--base) * 3);
   padding: calc(var(--base) * 0.08);
   outline: none;
   border: calc(var(--base) * 0.01) solid transparent;
   border-radius: calc(var(--base) * 0.04);
   font: inherit;
+  font-size: 20px;
   letter-spacing: inherit;
   color: inherit;
   background-color: white;
@@ -239,16 +299,23 @@ const deleteComment = (commentIndex) => {
   flex-direction: column;
   align-items: flex-start;
   gap: 16px;
-  margin-top: 16px;
+  margin-top: 64px;
 }
 
 .short-p {
   word-wrap: break-word;
   word-break: break-all;
+  white-space: pre-wrap;
 }
 
 .full-p {
   word-wrap: break-word;
   word-break: break-all;
+  white-space: pre-wrap;
+}
+
+.post-buttons {
+  display: flex;
+  gap: 16px;
 }
 </style>
